d302cbce8e8d4f515b4755e047b80d76bca9fbc4
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-10-09 20:56:41 +0100

    Fix type hints

--- pytest_regressions-stubs/__init__.pyi
+++ pytest_regressions-stubs/__init__.pyi
@@ -2,7 +2,7 @@
 from typing import Any, Callable, Type
 
 
-def add_custom_yaml_representer(data_type: Type, representer_fn: Callable[[Any], Any]):
+def add_custom_yaml_representer(data_type: Type, representer_fn: Callable[[Any, Any], Any]):
 	"""
 	:param callable representer_fn: Function that receives object of `data_type` type as
 		argument and must return a YAML-convertible representation.
